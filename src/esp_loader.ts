d81c6ba0e4e3a82c1d2aaf037497276e5984b397
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-12-16 14:42:34 +0100

    Throw error if synchronization to ESP fails
    Added error handling for ESP synchronization failure.

--- src/esp_loader.ts
+++ src/esp_loader.ts
@@ -850,8 +850,10 @@ export class ESPLoader extends EventTarget {
       this._inputBuffer.length = 0;
       const response = await this._sync();
       if (response) {
+        await sleep(SYNC_TIMEOUT);
         return true;
       }
+      await sleep(SYNC_TIMEOUT);
     }
 
     throw new Error("Couldn't sync to ESP. Try resetting.");
